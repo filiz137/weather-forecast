61c1b17cbd2b80bf4cd64ff582dcf79f210fe836
Filiz Altıntürk <info@filizaltinturk.com>
2017-07-13 23:40:55 +0300

    city name, temp and weather icon added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,16 @@ var forecast = new Vue({
     data: {
         APPID: 'b712b9a9b0785a9842d7c7ba49f37a5f',
         openweathermapAPI: 'http://api.openweathermap.org/data/2.5/',
-        main: {
-            temp: null
-        },
-        name: null
+        weather: [],
+        cities: []
     },
     methods: {
         forecastCity: function() {
-            var url = this.openweathermapAPI + 'weather?q=London,uk&appid=' + this.APPID;
+            var url = this.openweathermapAPI + 'group?id=2643743,2968815,6356055,3169069,2759794&units=metric&appid=' + this.APPID;
             this.$http.get(url).then(result => {
 
                 // get body data
-                this.weather = result.body;
+                this.weather = result.body.list;
 
             }, result => {
                 console.log('error!')
