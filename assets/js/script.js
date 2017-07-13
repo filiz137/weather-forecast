0ab293442e15a0f7019e96fe1efc99045e100bc6
Filiz Altıntürk <filiz.altinturk@nesine.com>
2017-07-13 16:25:58 +0300

    api call

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+
+var forecast = new Vue({
+    el: '#main',
+    data: {
+        APPID: 'b712b9a9b0785a9842d7c7ba49f37a5f',
+        openweathermapAPI: 'http://api.openweathermap.org/data/2.5/',
+        toggle: false,
+        errMsg: false,
+        main: {
+            temp: null
+        },
+        name: null
+    },
+    methods: {
+        forecastCity: function() {
+            var url = this.openweathermapAPI + 'weather?q=London,uk&appid=' + this.APPID;
+            this.$http.get(url).then(result => {
+
+                // get body data
+                this.weather = result.body;
+
+            }, result => {
+                console.log('error!')
+            });
+        }
+
+
+    }
+});
+forecast.forecastCity();
+
+
